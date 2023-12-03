ea97b074e91438ad3dac1bb3106c96acac8344c6
Nikita Sivukhin <sivukhin@sharechat.co>
2023-12-03 13:30:41 +0400

    day3

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,6 @@ const DAY1_HARD_PATTERN: &[&[&str]; 10] = &[
 fn day1(input: &str, patterns: &[&[&str]; 10]) {
     let mut sum = 0;
     for line in input.lines() {
-        if line.len() == 0 {
-            continue;
-        }
         let (mut first_digit, mut last_digit) = (None::<i32>, None::<i32>);
         for i in 0..line.len() {
             'digit: for digit in 0..10 {
@@ -61,9 +58,6 @@ fn day2(input: &str, bag: Bag) {
     let mut count_valid = 0;
     let mut power_sum = 0;
     for line in input.lines() {
-        if line.len() == 0 {
-            continue;
-        }
         let game = line.strip_prefix("Game ").unwrap();
         let (game_id, rounds) = game.split_once(':').unwrap();
         let mut valid = true;
@@ -97,8 +91,159 @@ fn day2(input: &str, bag: Bag) {
     eprintln!("day2: {power_sum}");
 }
 
+struct Grid<'a> {
+    table: &'a [u8],
+    width: i32,
+    height: i32,
+}
+
+impl<'a> Grid<'a> {
+    pub fn new(table: &'a [u8]) -> Self {
+        let (mut width, mut height) = (0, 0);
+        for i in 0..table.len() {
+            if table[i] == '\n' as u8 {
+                width = if width == 0 { (i + 1) as i32 } else { width };
+                height += 1;
+            }
+        }
+        Grid{ table: table, width: width, height: height - 1 }
+    }
+    pub fn at(&self, row: i32, col: i32) -> u8 {
+        let pos = self.pos(row, col);
+        if pos.is_none() { return '.' as u8; }
+        let symbol = self.table[pos.unwrap()] as u8;
+        return if symbol == '\n' as u8 { '.' as u8 } else { symbol };
+    }
+    pub fn pos(&self, row: i32, col: i32) -> Option<usize> {
+        if row < 0 || col < 0 || row >= self.height || col >= self.width { return None::<usize>; }
+        return Some((row * self.width + col) as usize);
+    }
+}
+
+trait ByteExtensions {
+    fn digit(&self) -> Option<i32>;
+    fn dot(&self) -> bool;
+}
+impl ByteExtensions for u8 {
+    fn digit(&self) -> Option<i32> { if '0' as u8 <= *self && *self <= '9' as u8 { Some((*self - '0' as u8) as i32) } else { None::<i32> } }
+    fn dot(&self) -> bool { *self == '.' as u8 }
+}
+trait ByteSeqExtensions {
+    fn number_span(&self, mid: usize) -> Option<i32>;
+}
+impl ByteSeqExtensions for &[u8] {
+    fn number_span(&self, mid: usize) -> Option<i32> {
+        if self[mid].digit().is_none() {
+            return None::<i32>;
+        }
+
+        let mut current = mid;
+        while current > 0 && self[current - 1].digit().is_some() {
+            current -= 1;
+        }
+        let mut number = 0;
+        while current < self.len() {
+            match self[current].digit() {
+                Some(digit) => { number = 10 * number + digit; },
+                None => return Some(number),
+            }
+            current += 1;
+        }
+        return Some(number);
+    }
+}
+
+fn day3_easy(input: &[u8]) {
+    let grid = Grid::new(input);
+    let mut sum = 0;
+    for row in 0..grid.height {
+        let mut col = 0;
+        while col < grid.width {
+            if grid.at(row, col).digit().is_none() {
+                col += 1;
+                continue;
+            }
+            let (mut number, mut is_part_number) = (0, !grid.at(row, col - 1).dot());
+            while col < grid.width {
+                let symbol = grid.at(row, col).digit();
+                if symbol.is_none() {
+                    break;
+                }
+                number = 10 * number + symbol.unwrap();
+                for (drow, dcol) in [(-1, -1), (-1, 0), (-1, 1), (1, -1), (1, 0), (1, 1)] {
+                    is_part_number |= !grid.at(row + drow, col + dcol).dot();
+                }
+                col += 1;
+            }
+            is_part_number = is_part_number || !grid.at(row, col).dot();
+            if !is_part_number {
+                continue;
+            }
+            sum += number;
+        }
+    }
+    eprintln!("day3: {sum}");
+}
+
+struct Gear {
+    drivers_count: usize,
+    drivers: [i32; 2],
+}
+
+impl Gear {
+    pub fn add_driver(&mut self, driver: Option<i32>) {
+        if self.drivers_count >= 2 || driver.is_none() {
+            return;
+        }
+        self.drivers[self.drivers_count] = driver.unwrap();
+        self.drivers_count += 1;
+    }
+    pub fn power(&self) -> Option<i32> {
+        if self.drivers_count == 2 { Some(self.drivers[0] * self.drivers[1]) }
+        else { None::<i32> }
+    }
+}
+
+fn day3_hard(input: &[u8]) {
+    let grid = Grid::new(input);
+    let mut sum = 0;
+    for row in 0..grid.height {
+        for col in 0..grid.width {
+            if grid.at(row, col) != '*' as u8 {
+                continue;
+            }
+            let mut gear = Gear{drivers_count: 0, drivers: [0; 2]};
+            if let Some(pos) = grid.pos(row, col - 1) {
+                gear.add_driver(input.number_span(pos));
+            }
+            if let Some(pos) = grid.pos(row, col + 1) {
+                gear.add_driver(input.number_span(pos));
+            }
+            for drow in [-1, 1] {
+                if let Some(pos) = grid.pos(row + drow, col) {
+                    let driver = input.number_span(pos);
+                    gear.add_driver(driver);
+                    if driver.is_some() {
+                        continue;
+                    }
+                }
+                if let Some(pos) = grid.pos(row + drow, col - 1) {
+                    gear.add_driver(input.number_span(pos));
+                }
+                if let Some(pos) = grid.pos(row + drow, col + 1) {
+                    gear.add_driver(input.number_span(pos));
+                }
+            }
+            sum += gear.power().unwrap_or(0);
+        }
+    }
+    eprintln!("day3: {sum}");
+}
+
 fn main() {
-    day1(include_str!("inputs/input01.txt"), DAY1_EASY_PATTERN);
-    day1(include_str!("inputs/input01.txt"), DAY1_HARD_PATTERN); 
-    day2(include_str!("inputs/input02.txt"), Bag{ red: 12, green: 13, blue: 14 });
+    day1(include_str!("inputs/input01.txt").trim(), DAY1_EASY_PATTERN);
+    day1(include_str!("inputs/input01.txt").trim(), DAY1_HARD_PATTERN); 
+    day2(include_str!("inputs/input02.txt").trim(), Bag{ red: 12, green: 13, blue: 14 });
+    day3_easy(include_bytes!("inputs/input03.txt"));
+    day3_hard(include_bytes!("inputs/input03.txt"));
 }
